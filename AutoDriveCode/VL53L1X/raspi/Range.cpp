1ce46fda24d31ea9c2e6f2b9567ffc926e39ce6e
yooshima <o.shimayu891@gmail.com>
2025-12-10 12:46:52 +0900

    chore: format

--- AutoDriveCode/VL53L1X/raspi/Range.cpp
+++ AutoDriveCode/VL53L1X/raspi/Range.cpp
@@ -10,37 +10,32 @@ VL53L1X Distance_Sensor;
 
 int main()
 {
-	
-	
-   bool status;  
-   status = Distance_Sensor.begin();
+  bool status;
+  status = Distance_Sensor.begin();
   // cout << status << endl;
   int counter = 0;
   int rate = 0;
   double time = 0;
   clock_t begin = clock();
 
-while(counter < 500){
+  while(counter < 500){
+    Distance_Sensor.startMeasurement(); //Write configuration bytes to initiate measurement
 
-   
-   Distance_Sensor.startMeasurement(); //Write configuration bytes to initiate measurement
+    //Poll for completion of measurement. Takes 40-50ms.
+    while(Distance_Sensor.newDataReady() == false){
+      usleep(5);
+    }
 
-  //Poll for completion of measurement. Takes 40-50ms.
-  while(Distance_Sensor.newDataReady() == false){
-    usleep(5);
- }   
-
-
-  int distance = Distance_Sensor.getDistance(); //Get the result of the measurement from the sensor
-  cout << "Distance(mm) :"<< distance << endl;
-  counter++;
-
-}
+    int distance = Distance_Sensor.getDistance(); //Get the result of the measurement from the sensor
+    cout << "Distance(mm) :"<< distance << endl;
+    counter++;
+  }
 
   clock_t end = clock();
   time = begin - end;
   rate = 50/time;
   cout << time;
- return 0;
+
+  return 0;
 }
 
